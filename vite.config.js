2c1a619fb7fbbd470530fd1f82bfac840536020f
DayhawkM <d025475m@student.staffs.ac.uk>
2024-04-19 21:17:13 +0100

    first commit

--- vite.config.js
+++ vite.config.js
@@ -11,11 +11,4 @@ export default defineConfig({
             refresh: true,
         }),
     ],
-    server: {
-        hmr: {
-            host: 'localhost',
-        },
-        watch: {
-            usePolling: true,
-        }
-    },})
+});
