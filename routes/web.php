5dec789de64ad224fdafc836dbc8b42114f8a12e
DayhawkM <d025475m@student.staffs.ac.uk>
2024-04-20 15:29:38 +0100

    week 4

--- routes/web.php
+++ routes/web.php
@@ -23,11 +23,23 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::resource('chirps', ChirpController::class)
-    ->only(['index', 'store', 'edit', 'update', 'destroy'])
-    ->middleware(['auth', 'verified']); 
 
-    Route::middleware(['auth', 'verified'])->group(function () {
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::resource('chirps', ChirpController::class)
+        ->only(['index', 'store', 'edit', 'update', 'destroy']);
+    
+        Route::post(
+            '/chirps/{chirp}/addToFavourites',
+            [ChirpController::class, 'addToFavourites']
+         )->name('chirps.favourites.add');
+         Route::get(
+            '/chirps/favourites',
+            [ChirpController::class, 'favourites']
+         )->name('chirps.favourites');
+      });
+      
+
+Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
